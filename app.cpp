cd6b522613f065a8785b0134cb3fb3ab78f62e70
mimyutt <mutiakhofifaturr@gmail.com>
2026-02-23 12:01:24 +0700

    menambahkan standar library

---
+++ app.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+int main() // program utama
+{
+    //manampilkan string
+    cout <<"selamat datang di TI UMY"
+    // manampilkan numeris
+    cout << 10;
+}
